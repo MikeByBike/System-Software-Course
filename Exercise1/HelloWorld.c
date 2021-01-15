3fe7a39694a1366e97c9c45e05f4de98c29bdebc
MikeByBike <michalis.iona@edu.turkuamk.fi>
2021-01-15 15:45:13 +0200

    Adding gitignore and Exercise1

---
+++ Exercise1/HelloWorld.c
@@ -0,0 +1,10 @@
+//File name:	HelloWorld.c
+//Author:	Michalis Iona
+//Description:	Printing Hello World
+
+#include <stdio.h> //this is included to be able to use command printf
+int main() {     
+	printf("Hello World"); //printing Hello World
+	printf("CTU24"); //printing CTU24
+	return 0; //return 0 to stop program  
+	}
