17487692155ff2e35de798440e7dca80d357e9d9
MikeByBike <michalis.iona@edu.turkuamk.fi>
2021-01-21 17:21:07 +0200

    Updates Exercise 2

--- Exercise2/Exercise2.c
+++ Exercise2/Exercise2.c
@@ -3,22 +3,51 @@
 //Description:	The user enters two integers which are manipulated to do certain tasks 
 
 #include <stdio.h>
+#include <math.h>
 
 int main() {
-int n1, n2; //variables
+int n1, n2; //variable
+int diff; //variable
+int sum; //variable
+double div; //variable
+double avg; //variable
+long long power = 1; //variable
 	printf("Enter two numbers: ");
 	scanf("%d%d", &n1, &n2); //reads both numbers
     	
-    	if (n1 == n2)
-    	   printf("Both numbers are equal to each other");
-    	if (n1 > n2)
-    	{
-    	   printf("Largest number is %d.", n1)
+    	if (n1 == n2) //checks if first number is equal with the second number
+    	   printf("Both numbers are equal to each other \n");
+    	   
+    	if (n1 > n2) { //checks if first number is biggger than the second number
+     	   diff = n1-n2; //finds difference between first number and second number if the first number is bigger
+     	   div = (float)n1/n2; //divides first number by second number as first number is bigger
+    	   printf("The largest number is: %d\n", n1);
     	}
-    	else
-    	{
-    	   printf("Largest number is %d.", n2)
+    	else { //checks if second number is biggger than first
+    	   diff=n2-n1; //finds difference between first number and second number if the second number is bigger
+    	   div = (float)n2/n1; //divides second number by first number as second number is bigger
+    	   printf("The largest number is: %d\n", n2);
     	}
     	
+    	sum = n1 + n2;//sum of two numbers
+    	printf("The sum of two numbers: %d\n", sum);
+    	
+    	avg = (float)(n1+n2)/2; //average of two numbers
+    	printf("The average of two numbers: %.2lf\n", avg);
+    	
+        printf("The difference of two numbers is: %d\n", diff);
+        
+	if (n1 != 0 || n2 != 0) { //if either number is 0 defines that its not possible to divide
+	printf("The division is not possible \n");
+	}
+	else { //if none of the numbers is 0 then posibility of division occurs 
+        printf("The division between two numbers is: %.2lf\n", div);
+        }
+        
+    	while (n2 != 0) { //makes sure that the exponent is not 0
+    	   power *= n1; //ensures that first number is base
+    	   --n2; //ensures that second number is exponent
+    	}
+    	printf("The second number power of your first number is: %lld\n", power);
     	
 }
